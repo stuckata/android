321f1debfe0d860b5f745a6e465949141926983c
stuckata <stu.kirkov@gmail.com>
2015-09-29 17:34:52 +0300

    starting another activity

--- MyFirstApp/app/src/main/java/stuckata/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/stuckata/myfirstapp/MainActivity.java
@@ -1,11 +1,15 @@
 package stuckata.myfirstapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "stuckata.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +37,14 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
